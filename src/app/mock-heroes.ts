b4f8e9abbab0f2747239e4c7bdb0221f2b955392
StefanFocht <101522938+StefanFocht@users.noreply.github.com>
2022-05-16 11:04:47 +0200

    workingApp

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./heroes/hero";
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Dr.Nice'},
+    {id: 12, name: 'Marco'},
+    {id: 13, name: ' Bombasto '},
+    {id: 14, name: ' Corny '},
+    {id: 15, name: ' Ton '},
+    {id: 16, name: ' Ont '},
+    {id: 17, name: 'dann  '},
+    {id: 18, name: 'sam'},
+    {id: 19, name: 'Jan'},
+    {id: 20, name: ' Alex '}
+];
